319b25f6f98738a65b49638e6cabd2a0f7319076
Ariane Evans <ariane.evans@student.makeschool.com>
2020-11-30 23:35:52 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from "axios";
 import './App.css';
 
 function App() {
+  let [text, setText] = useState("");
+
+  const options = {
+    method: 'POST',
+    url: 'https://yodish.p.rapidapi.com/yoda.json',
+    params: {text: 'Master Obiwan has lost a planet.'},
+    headers: {
+      'x-rapidapi-key': '5d0b8f28c8msh35ad9af1178a47ep192210jsn7ee9b426ee33',
+      'x-rapidapi-host': 'yodish.p.rapidapi.com'
+    }
+  };
+
+  const onTranslateClick = () =>  {
+    axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Yoda speech translator!</h1>
+      <label>English</label>
+      <br/>
+      <input type="text" value={text} onChange={ e => setText(e.target.value)} placeholder="Type your text here..."/>
+      <button onClick={onTranslateClick}>Translate</button>
+      <br/>
+      <label>Yoda</label>
+      <div>yoda translator will be here</div>
     </div>
   );
 }
