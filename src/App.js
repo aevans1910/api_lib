d959efe02d9623350e35d17c5b41b52f74ede8f4
Ariane Evans <ariane.evans@student.makeschool.com>
2020-12-03 16:33:41 -0800

    [add] api

--- src/App.js
+++ src/App.js
@@ -4,35 +4,47 @@ import './App.css';
 
 function App() {
   let [text, setText] = useState("");
-
-  const options = {
-    method: 'POST',
-    url: 'https://yodish.p.rapidapi.com/yoda.json',
-    params: {text: 'Master Obiwan has lost a planet.'},
-    headers: {
-      'x-rapidapi-key': '5d0b8f28c8msh35ad9af1178a47ep192210jsn7ee9b426ee33',
-      'x-rapidapi-host': 'yodish.p.rapidapi.com'
-    }
-  };
+  let [yodaText, setYodaText] = useState("");
 
   const onTranslateClick = () =>  {
-    axios.request(options).then(function (response) {
-	console.log(response.data);
-}).catch(function (error) {
-	console.error(error);
-});
+    axios
+      .post("/translate/yoda.json", { text })
+      .then(res => {
+        const { translated } = res.data.contents;
+        setYodaText(translated);
+      })
+      .catch(err => {
+        console.log(err);
+      })
   };
 
   return (
     <div>
-      <h1>Yoda speech translator!</h1>
-      <label>English</label>
-      <br/>
-      <input type="text" value={text} onChange={ e => setText(e.target.value)} placeholder="Type your text here..."/>
-      <button onClick={onTranslateClick}>Translate</button>
-      <br/>
-      <label>Yoda</label>
-      <div>yoda translator will be here</div>
+      <div className="container">
+        <div className="hero">
+          <h1>Yoda Speech Translator</h1>
+          <p>Translate anything into Yoda speech</p>
+        </div>
+        <div className="about">
+          <img src="images/yoda.jpg" alt="Yoda"/>
+          <div className="text">
+            <h2>About the Yoda Speech Translator</h2>
+            <p>The Yoda Speech Translator is an API that converts phrases into 
+              how Yoda would say them. We have simplified its use for you by
+              creating an easy to use translator component. Input whatever
+              sentence or phrase you want into the input box, hit the Translate
+              button, and get your Yoda translation!
+            </p>
+          </div>
+        </div>
+        <label>English</label>
+        <br/>
+        <input type="text" value={text} onChange={ e => setText(e.target.value)} placeholder="Type your text here..."/>
+        <button onClick={onTranslateClick}>Translate</button>
+        <br/>
+        <label>Yoda</label>
+        <div>{yodaText}</div>
+      </div>
     </div>
   );
 }
